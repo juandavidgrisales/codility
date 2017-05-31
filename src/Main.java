72e0b42e838d90afe1005ec4b7e69ba78c813342
Juan David Grisales <juandavid.grisales@gmail.com>
2017-05-31 10:15:26 -0500

    add first challenge, Binary Gap

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import Iterations.BinaryGap;
+
+/**
+ * Created by juandavidgrisales on 31/05/17.
+ */
+public class Main {
+
+    public static void main (String[] argsv) {
+        //Lesson 1
+        //Iterations
+        new BinaryGap();
+    }
+
+}
